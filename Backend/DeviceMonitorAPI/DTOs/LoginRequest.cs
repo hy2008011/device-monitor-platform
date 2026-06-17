05c35c858779d202b455bf0bcd2085b35f8f0fb9
hy2008011 <hy2008_011@hotmail.com>
2026-06-17 15:39:28 +0800

    Create LoginRequest.cs

---
+++ Backend/DeviceMonitorAPI/DTOs/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace DeviceMonitorAPI.DTOs;
+
+public class LoginRequest
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
