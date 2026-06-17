0ecd430152496e7ab5bea4c35de21b97def60246
hy2008011 <hy2008_011@hotmail.com>
2026-06-17 15:59:47 +0800

    Delete Backend/DeviceMonitorAPI/Models/LoginRequest.cs

--- Backend/DeviceMonitorAPI/Models/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace DeviceMonitorAPI.DTOs;
-
-public class LoginRequest
-{
-    public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
